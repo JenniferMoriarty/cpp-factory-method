98df6a879aba5557036e5eb45dd02eda0edfc6b3
DrJMoriarty <jmoriar@dpsk12.net>
2023-01-23 14:58:48 -0700

    Add project files.

---
+++ c++ inheritance/factory.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"gameobject.h"
+//these functions help us dynamically create duck and star objects 
+class factory {
+public:
+    virtual gameObject* create() = 0;
+};
+class StarFactory :public factory {
+public:
+    star* create() { return new star(); }
+};
+class DuckFactory :public factory {
+public:
+    duck* create() { return new duck(); }
+};
